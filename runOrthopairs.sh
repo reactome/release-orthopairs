1b2d565391ad7b61feb5ea5a83335a08ebdc488b
Justin Cook <cookersjs@gmail.com>
2020-05-07 12:56:53 -0400

    Quotes removal

--- runOrthopairs.sh
+++ runOrthopairs.sh
@@ -14,7 +14,7 @@ wget ftp://ftp.pantherdb.org/ortholog/current_release/QfO_Genome_Orthologs.tar.g
 tar -xvf QfO_Genome_Orthologs.tar.gz
 
 echo "Downloading http://www.informatics.jax.org/downloads/reports/HGNC_homologene.rpt..."
-wget -O "mmus_alternate_ids.txt" http://www.informatics.jax.org/downloads/reports/HGNC_homologene.rpt
+wget -O mmus_alternate_ids.txt http://www.informatics.jax.org/downloads/reports/HGNC_homologene.rpt
 
 echo "Downloading ftp://ftp.rgd.mcw.edu/pub/data_release/GENES_RAT.txt..."
 wget -O rnor_alternate_ids.txt ftp://ftp.rgd.mcw.edu/pub/data_release/GENES_RAT.txt
