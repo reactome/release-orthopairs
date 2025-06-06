52570f3fe744f97357c44ca4cb9dfd937329e639
jweiser <joel.weiser@oicr.on.ca>
2025-06-06 18:14:08 -0400

    adds option for Java API retrieval of gene names

--- src/main/java/org/reactome/release/orthopairs/UniProtGeneNamesRetriever.java
+++ src/main/java/org/reactome/release/orthopairs/UniProtGeneNamesRetriever.java
@@ -3,6 +3,17 @@ package org.reactome.release.orthopairs;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import uk.ac.ebi.kraken.interfaces.uniprot.Gene;
+import uk.ac.ebi.uniprot.dataservice.client.Client;
+import uk.ac.ebi.uniprot.dataservice.client.QueryResult;
+import uk.ac.ebi.uniprot.dataservice.client.QueryResultPage;
+import uk.ac.ebi.uniprot.dataservice.client.ServiceFactory;
+import uk.ac.ebi.uniprot.dataservice.client.exception.ServiceException;
+import uk.ac.ebi.uniprot.dataservice.client.uniprot.UniProtComponent;
+import uk.ac.ebi.uniprot.dataservice.client.uniprot.UniProtQueryBuilder;
+import uk.ac.ebi.uniprot.dataservice.client.uniprot.UniProtService;
+import uk.ac.ebi.uniprot.dataservice.query.Query;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,7 +31,13 @@ public class UniProtGeneNamesRetriever {
     private static final Logger logger = LogManager.getLogger();
 
     private static final String UNIPROT_ID_MAPPING_URL = "https://rest.uniprot.org/idmapping";
+    private static final int MAX_NUMBER_OF_RETRIES = 5;
     private static final int UNIPROT_IDENTIFIER_BATCH_SIZE = 500;
+
+    public enum RetrievalMethod {
+        REST_API,
+        JAVA_API
+    }
 
     /**
      * Queries the UniProt mapping service through their REST API. All Uniprot accession IDs are taken from
@@ -31,14 +48,15 @@ public class UniProtGeneNamesRetriever {
      * @param pantherHomologMappings Map<String, Set<String>> - Species-specific homolog mappings.
      * @throws InterruptedException - Thrown if the Sleep process that occurs after every batch query is interrupted.
      * @throws IOException - Thrown if unable to create/write to the mapping files or if curl process to query
+     * @throws ServiceException - Thrown by UniProtService class if service is unavailable.
      * REST API throws an exception
      */
     public static void retrieveAndStoreGeneNameMappings(
-        String speciesKey, String releaseNumber, Map<String, Set<String>> pantherHomologMappings)
-        throws IOException, InterruptedException {
+        String speciesKey, String releaseNumber, Map<String, Set<String>> pantherHomologMappings, RetrievalMethod retrievalMethod)
+		throws IOException, InterruptedException, ServiceException {
 
         // Get all gene names associated with all UniProt identifiers.
-        Set<String> uniprotAccessionsToGeneNames = retrieveGeneNameMappings(pantherHomologMappings);
+        Set<String> uniprotAccessionsToGeneNames = retrieveGeneNameMappings(pantherHomologMappings, retrievalMethod);
         // Store results to {targetSpecies}_gene_name_mapping.tsv file.
         storeGeneNameMappings(speciesKey, releaseNumber, uniprotAccessionsToGeneNames);
     }
@@ -50,14 +68,21 @@ public class UniProtGeneNamesRetriever {
      * @return Set<String> - UniProt-Gene name mappings that will be written to file.
      * @throws IOException - Thrown if curl process to query REST API throws an exception
      * @throws InterruptedException - Thrown if the Sleep process that occurs after every batch query is interrupted.
+     * @throws ServiceException - Thrown by UniProtService class if service is unavailable.
      */
-    private static Set<String> retrieveGeneNameMappings(Map<String, Set<String>> pantherMappings)
-        throws IOException, InterruptedException {
+    private static Set<String> retrieveGeneNameMappings(Map<String, Set<String>> pantherMappings, RetrievalMethod retrievalMethod)
+		throws IOException, InterruptedException, ServiceException {
         // Get all UniProt identifiers and organize them into a List of Sets that contain 250 UniProt identifiers each.
         List<Set<String>> partitionedUniProtIds = getPartitionedUniProtIdentifiers(pantherMappings);
         //System.out.println(partitionedUniProtIds);
         // Query UniProt for gene names associated with UniProt identifiers.
-        return retrieveGeneNamesFromUniProt(partitionedUniProtIds);
+        if (retrievalMethod.equals(RetrievalMethod.REST_API)) {
+            return retrieveGeneNamesFromUniProtRESTAPI(partitionedUniProtIds);
+        } else if (retrievalMethod.equals(RetrievalMethod.JAVA_API)) {
+            return retrieveGeneNamesFromUniProtViaJavaAPI(partitionedUniProtIds);
+        } else {
+            throw new RuntimeException("UniProt Gene Name retrieval method unknown: " + retrievalMethod);
+        }
     }
 
     /**
@@ -78,7 +103,7 @@ public class UniProtGeneNamesRetriever {
      * @param pantherMappings Map<String, Set<String>> - Species-specific homolog mappings.
      * @return Set<String> - All UniProt identifiers in the mapping object.
      */
-    public static Set<String> getUniProtIdentifiers(Map<String, Set<String>> pantherMappings) {
+    static Set<String> getUniProtIdentifiers(Map<String, Set<String>> pantherMappings) {
         Set<String> uniprotIds = new HashSet<>();
         for (String uniprotKey : pantherMappings.keySet()) {
             for (String uniprotValue : pantherMappings.get(uniprotKey)) {
@@ -97,7 +122,7 @@ public class UniProtGeneNamesRetriever {
      * @param uniprotIds Set<String> - All unique UniProt identifiers that were in the Panther mapping.
      * @return List<Set<String>> Partitioned UniProt identifiers.
      */
-    public static List<Set<String>> partitionUniProtIdentifiers(Set<String> uniprotIds) {
+    static List<Set<String>> partitionUniProtIdentifiers(Set<String> uniprotIds) {
         List<Set<String>> partitionedUniProtIds = new ArrayList<>();
         Set<String> partition = new HashSet<>();
         for (String uniprotId : uniprotIds) {
@@ -120,7 +145,7 @@ public class UniProtGeneNamesRetriever {
      * @throws IOException - Thrown if curl process to query REST API throws an exception
      * @throws InterruptedException - Thrown if the Sleep process that occurs after every batch query is interrupted.
      */
-    public static Set<String> retrieveGeneNamesFromUniProt(List<Set<String>> partitionedUniProtIds) throws
+    private static Set<String> retrieveGeneNamesFromUniProtRESTAPI(List<Set<String>> partitionedUniProtIds) throws
 		IOException, InterruptedException {
 
         Set<String> uniprotAccessionsToGeneNames = new HashSet<>();
@@ -140,7 +165,93 @@ public class UniProtGeneNamesRetriever {
                 String uniprotAccessionToGeneName = accession + "\t" + geneName + "\n";
                 uniprotAccessionsToGeneNames.add(uniprotAccessionToGeneName);
             }
+
+            for (String uniProtIdentifier : uniProtIdentifiersBatch) {
+                if (!results.containsKey(uniProtIdentifier)) {
+                    Files.write(
+                        Paths.get("not_mapped.tsv"),
+                        uniProtIdentifier.concat(System.lineSeparator()).getBytes(),
+                        StandardOpenOption.CREATE, StandardOpenOption.APPEND
+                    );
+                }
+            }
         }
+
+        return uniprotAccessionsToGeneNames;
+    }
+
+    /**
+     * Query UniProt via the Java API for gene names using UniProt accession IDs.
+     * @param partitionedUniProtIds List<Set<String>> Partitioned UniProt identifiers.
+     * @return Set<String> - Accession-to-Name mappings that will be stored to a file.
+     * @throws ServiceException - Thrown by UniProtService class if service is unavailable.
+     * @throws InterruptedException - Thrown if the Sleep process that occurs after every batch query is interrupted.
+     */
+    private static Set<String> retrieveGeneNamesFromUniProtViaJavaAPI(List<Set<String>> partitionedUniProtIds) throws ServiceException, InterruptedException {
+        // Create the UniProtService.
+        ServiceFactory serviceFactoryInstance = Client.getServiceFactoryInstance();
+        UniProtService uniprotService = serviceFactoryInstance.getUniProtQueryService();
+        uniprotService.start();
+        Set<String> uniprotAccessionsToGeneNames = new HashSet<>();
+        for (Set<String> uniprotIdentifierPartition : partitionedUniProtIds) {
+            // Build UniProt API query from Set of 250 UniProt identifiers.
+            Query query = UniProtQueryBuilder.accessions(uniprotIdentifierPartition);
+            // Perform UniProt API query to retrieve gene names associated with identifiers.
+
+            QueryResult<UniProtComponent<Gene>> uniprotEntries = null;
+            int currentQueryNum = 1;
+            while (currentQueryNum < MAX_NUMBER_OF_RETRIES && uniprotEntries == null) {
+                try {
+                    uniprotEntries = uniprotService.getGenes(query);
+                } catch (ServiceException e) {
+                    logger.error(e);
+                    currentQueryNum += 1;
+                    Thread.sleep(5000);
+                }
+            }
+
+            if (uniprotEntries == null) {
+                throw new RuntimeException("Unable to get Uniprot Entries after " + MAX_NUMBER_OF_RETRIES + " tries");
+            }
+
+
+            QueryResultPage<UniProtComponent<Gene>> uniprotEntriesResultPage = uniprotEntries.getCurrentPage();
+            int pageCount = 0;
+            int totalHitCount = 0;
+            while (totalHitCount < uniprotEntries.getNumberOfHits()) {
+                while (pageCount < uniprotEntriesResultPage.pageSize()) {
+
+                    UniProtComponent<Gene> geneObject;
+                    try {
+                        geneObject = uniprotEntriesResultPage.getResult(pageCount);
+                    } catch (Exception e) {
+                        pageCount += 1;
+                        totalHitCount += 1;
+                        continue;
+                    }
+
+                    pageCount += 1;
+                    totalHitCount += 1;
+
+                    if (!geneObject.getComponent().isEmpty()) {
+                        // Iterate through all Gene components in the response.
+                        for (Gene geneComponent : geneObject.getComponent()) {
+                            // Tab-separate UniProt accession ID and its associated gene name, and then store these in the Set that will be returned.
+                            uniprotAccessionsToGeneNames.add(geneObject.getAccession().toString() + "\t" + geneComponent.getGeneName().toString() + "\n");
+                        }
+                    }
+
+
+                    if (totalHitCount % 1000 == 0) {
+                        logger.info(totalHitCount + " UniProt identifiers have been queried for gene names");
+                    }
+                }
+                uniprotEntriesResultPage = uniprotEntriesResultPage.fetchNextPage();
+                pageCount = 0;
+            }
+
+        }
+        uniprotService.stop();
 
         return uniprotAccessionsToGeneNames;
     }
